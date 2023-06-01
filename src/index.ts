3ef7f735411d1a7d6ede777fc14875d8d825e779
Tetsuo Koyama <tkoyama010@gmail.com>
2023-06-01 21:57:19 +0900

    :rocket: Create project by copier

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { requestAPI } from './handler';
+
+/**
+ * Initialization data for the myextension extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'myextension:plugin',
+  description: 'A JupyterLab extension.',
+  autoStart: true,
+  activate: (app: JupyterFrontEnd) => {
+    console.log('JupyterLab extension myextension is activated!');
+
+    requestAPI<any>('get-example')
+      .then(data => {
+        console.log(data);
+      })
+      .catch(reason => {
+        console.error(
+          `The myextension server extension appears to be missing.\n${reason}`
+        );
+      });
+  }
+};
+
+export default plugin;
